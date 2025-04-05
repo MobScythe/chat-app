fadb8dc4f5495993c69322db0fec44760ff3f08b
MobScythe <mschaudhry04@gmail.com>
2025-04-06 01:59:30 +0500

    first commit

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.send("register route");
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
