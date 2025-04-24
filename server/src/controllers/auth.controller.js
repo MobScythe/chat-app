6ad66cdedfe1a0b1dc59e2852002284f6ad04d1d
Mohammed Shahbaz <mschaudhry04@gmail.com>
2025-04-24 20:49:46 +0500

    login and logout complete

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export const register = async (req, res) => {
   const { fullName, email, password } = req.body;
 
   if (!fullName || !email || !password) {
-    res.status(400).json({ message: "All fields must be required" });
+    return res.status(400).json({ message: "All fields must be required" });
   }
 
   if (password.length < 6) {
@@ -16,8 +16,8 @@ export const register = async (req, res) => {
   }
 
   try {
-    const user = await User.findOne({ email });
-    if (user) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
       return res
         .status(400)
         .json({ message: "user already exists with this email" });
@@ -47,10 +47,51 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields must be required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "invalid credentials" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log({ message: "Error in logout controller" });
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  const { email, fullName, password } = req.body;
+  if (!email || !fullName || !password) {
+    return res.status(400).json({ message: "All fields must be required" });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be atleast 6 characters" });
+  }
 };
